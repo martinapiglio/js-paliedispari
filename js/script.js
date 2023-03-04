5c115d890d02958d5b66645403189c2c1f1a11c8
martinapiglio <martina.piglio@gmail.com>
2023-03-04 10:01:07 +0100

    palindrome: styling+variables+button click js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-console.log('test');
+/*
+1 - create variables:
+    a - input for word (from HTML doc);
+    b - button (from HTML doc);
+    c - output element - showing id the word is palindrome or not (from HTML doc);
+2 - button click
+*/
+
+
+//VARIABLES
+let wordInput = document.getElementById('word-input');
+let wordButton = document.getElementById('word-button');
+let wordOutput = document.getElementById('word-output');
+
+//BUTTON CLICK
+wordButton.addEventListener('click', function() {
+
+                //CHECK
+                console.log('pulsante cliccato');
+                console.log(wordInput.value);
+                //CHECK
+            
+    
+
+});
